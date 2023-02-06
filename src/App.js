209089ffab33efc94ca14e37d4f21231b1fafbbf
anuragsingh6886 <anuragsingh@gmail.com>
2023-02-06 16:51:19 +0530

    First version 1.0.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World, Its SFCC Classroom!</h1>
+      <h3>Its a first line of a code in this project</h3>
+    </div>
+  );
+};
+
+export default App;
